c9c26cfa39024e9e83573325701c67bb4595c774
saicharith2012 <saicharithpalabindhela@gmail.com>
2025-09-20 12:34:12 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      hello, world!
+    </div>
+  );
+}
+
+export default App;
